a99992ed10435a85278ff3515d6aa3910bc5f77e
luispereira <luispereira268@gmail.com>
2016-01-22 08:56:32 +0000

    + Plugin in order to make the java compile (searching for the annotations)

---
+++ logthis-plugin/src/main/groovy/com/lib/logthisplugin/LogThisPlugin.groovy
@@ -0,0 +1,73 @@
+package com.lib.logthisplugin
+
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+
+class LogThisPlugin implements Plugin<Project> {
+    @Override void apply(Project project) {
+         def hasApp = project.plugins.withType(AppPlugin)
+         def hasLib = project.plugins.withType(LibraryPlugin)
+
+         if (!hasApp && !hasLib) {
+          throw new IllegalStateException("'android' or 'android-library' plugin required.")
+         }
+
+         final def variants
+         if (hasApp) {
+              variants = project.android.applicationVariants
+          } else {
+              variants = project.android.libraryVariants
+          }
+
+          project.dependencies {
+            debugCompile 'org.aspectj:aspectjrt:1.8.6'
+            compile 'com.github.luispereira:logthisannotations:0.4.+'
+          }
+
+          variants.all { variant ->
+            JavaCompile javaCompile = variant.javaCompile
+            javaCompile.doLast {
+                String[] args = [
+                "-showWeaveInfo",
+                "-1.5",
+                "-inpath", javaCompile.destinationDir.toString(),
+                "-aspectpath", javaCompile.classpath.asPath,
+                "-d", javaCompile.destinationDir.toString(),
+                "-classpath", javaCompile.classpath.asPath,
+                "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
+                ]
+
+                final def log = project.logger
+                log.debug "ajc args: " + Arrays.toString(args)
+
+                MessageHandler handler = new MessageHandler(true);
+                new Main().run(args, handler);
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                       case IMessage.ABORT:
+                        case IMessage.ERROR:
+                       case IMessage.FAIL:
+                            log.error message.message, message.thrown
+                            break;
+                        case IMessage.WARNING:
+                            log.warn message.message, message.thrown
+                            break;
+                        case IMessage.INFO:
+                            log.info message.message, message.thrown
+                            break;
+                        case IMessage.DEBUG:
+                            log.debug message.message, message.thrown
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
